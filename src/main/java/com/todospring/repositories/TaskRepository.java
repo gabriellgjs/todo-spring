6947d10f888096aa1e6ffc4bc73e7b53e0d7e492
gabriellgjs <contato.gabrieljosesilva@gmail.com>
2023-12-11 20:28:16 -0300

    refactor: import never used

--- src/main/java/com/todospring/repositories/TaskRepository.java
+++ src/main/java/com/todospring/repositories/TaskRepository.java
@@ -2,6 +2,5 @@ package com.todospring.repositories;
 
 import com.todospring.domain.task.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {}
