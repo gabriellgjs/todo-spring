2b3cc91babf1d088b4f1c309c1facbb87ceb26ad
gabriellgjs <contato.gabrieljosesilva@gmail.com>
2023-12-11 20:27:02 -0300

    refactor: refactor Task repository

--- src/main/java/com/todospring/repositories/TaskRepository.java
+++ src/main/java/com/todospring/repositories/TaskRepository.java
@@ -2,7 +2,6 @@ package com.todospring.repositories;
 
 import com.todospring.domain.task.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface TaskRepository extends JpaRepository<Task, Long> {
-
-}
+public interface TaskRepository extends JpaRepository<Task, Long> {}
