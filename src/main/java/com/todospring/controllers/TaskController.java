b31a3aba5602368a2ba3e75eb7f12ceb3c92dfb7
gabriellgjs <contato.gabrieljosesilva@gmail.com>
2023-12-11 20:27:14 -0300

    feat: create Task Controller

---
+++ src/main/java/com/todospring/controllers/TaskController.java
@@ -0,0 +1,31 @@
+package com.todospring.controllers;
+
+import com.todospring.domain.task.Task;
+import com.todospring.dtos.TaskDTO;
+import com.todospring.services.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    @Autowired
+    private TaskService taskService;
+
+    @PostMapping
+    public ResponseEntity<Task> createTask (@RequestBody TaskDTO task) {
+        Task newTask = this.taskService.createTask(task);
+
+        return new ResponseEntity<>(newTask, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Task>> getTasks () {
+        return new ResponseEntity<>(this.taskService.getAllTasks(), HttpStatus.OK);
+    }
+}
