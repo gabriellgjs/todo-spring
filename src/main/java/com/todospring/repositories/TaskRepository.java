e2e888aa99b90a190d71773c56186eb60eebcc59
gabriellgjs <contato.gabrieljosesilva@gmail.com>
2023-12-11 21:44:53 -0300

    refactor: public redundant

--- src/main/java/com/todospring/repositories/TaskRepository.java
+++ src/main/java/com/todospring/repositories/TaskRepository.java
@@ -4,5 +4,5 @@ import com.todospring.domain.task.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
-    public Task findTaskById(Long id);
+     Task findTaskById(Long id);
 }
