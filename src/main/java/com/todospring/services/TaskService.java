27cd8b62bc23aa586cdd9becf8488da5b244718a
gabriellgjs <contato.gabrieljosesilva@gmail.com>
2023-12-13 20:34:46 -0300

    refactor: import never used

--- src/main/java/com/todospring/services/TaskService.java
+++ src/main/java/com/todospring/services/TaskService.java
@@ -5,7 +5,6 @@ import com.todospring.domain.task.TaskStatus;
 import com.todospring.dtos.TaskDTO;
 import com.todospring.repositories.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
